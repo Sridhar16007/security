f28e646ba4d1d68d9adba59b883960748c5953a9
sridhar <sridhar200316@gmail.com>
2025-03-26 12:36:08 +0530

    added service and repo

--- src/main/java/com/security/Spring_Sec_Demo/DAO/UserRepo.java
+++ src/main/java/com/security/Spring_Sec_Demo/DAO/UserRepo.java
@@ -2,7 +2,9 @@ package com.security.Spring_Sec_Demo.DAO;
 
 import com.security.Spring_Sec_Demo.Model.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepo extends JpaRepository<Users,Integer> {
 
     Users findByUsername(String username);
